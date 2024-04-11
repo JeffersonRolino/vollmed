20d5469cbecae0c8bb4e78e7699ba3362ea5bd60
jeffersonrolino <jefferson.rolino@gmail.com>
2024-04-11 13:42:32 -0300

    Java Spring Boot - Curso 01 - Capítulo 05 -  Aula 03: Atualizando dados

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,16 +1,15 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
-import med.voll.api.medico.DadosCadastroMedico;
-import med.voll.api.medico.DadosListagemMedico;
-import med.voll.api.medico.Medico;
-import med.voll.api.medico.MedicoRepository;
+import med.voll.api.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/medicos")
@@ -32,7 +31,8 @@ public class MedicoController {
 
     @PutMapping
     @Transactional
-    public void atualizar(@RequestBody @Valid DadosCadastroMedico dados){
-
+    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados){
+        Optional<Medico> medicoSelecionado = repository.findById(dados.id());
+        medicoSelecionado.ifPresent(medico -> medico.atualizarInformacoes(dados));
     }
 }
