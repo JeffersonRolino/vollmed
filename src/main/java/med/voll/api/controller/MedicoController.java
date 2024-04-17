5f2422479eba8d0ad4034ae49777171f625fb872
jeffersonrolino <jefferson.rolino@gmail.com>
2024-04-17 12:22:51 -0300

    Java Spring Boot - Curso 02 - Capítulo 02 - Aula 04: Tratando erro 404

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,7 +1,7 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
-import med.voll.api.medico.*;
+import med.voll.api.domain.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -59,13 +59,8 @@ public class MedicoController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity detalhar(@PathVariable Long id){
-        Optional<Medico> medicoSelecionado = repository.findById(id);
-        if(medicoSelecionado.isPresent()){
-            return ResponseEntity.ok(new DadosDetalhamentoMedico(medicoSelecionado.get()));
-        }
-        else {
-            return ResponseEntity.notFound().build();
-        }
+    public ResponseEntity<DadosDetalhamentoMedico> detalhar(@PathVariable Long id){
+        var medico = repository.getReferenceById(id);
+        return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
     }
 }
--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,13 +1,18 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
-import med.voll.api.pacientes.*;
+import med.voll.api.domain.medico.DadosDetalhamentoMedico;
+import med.voll.api.domain.medico.Medico;
+import med.voll.api.domain.pacientes.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("pacientes")
